f2b64728a5dde65ce530c535916586b8c241441f
Mohamed Chaaben <69085814+mohamed-chaaben@users.noreply.github.com>
2025-05-16 21:35:46 -0400

    Update main.py
    Adding another tool: get_location

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from apis.reddit_api import get_reddit
 from apis.maps_api import get_directions
 from apis.news_api import get_news
 from apis.weather_api import get_weather
+from apis.need_location import get_location
 
 
 mcp = FastMCP('Demo')
@@ -13,6 +14,7 @@ get_weather = mcp.tool()(get_weather)
 get_news = mcp.tool()(get_news)
 get_directions = mcp.tool()(get_directions)
 get_reddit = mcp.tool()(get_reddit)
+get_location = mcp.tool()(get_location)
 
 
 
